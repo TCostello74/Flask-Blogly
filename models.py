c7bace0b25d50cc314a9a8e0b4a36d3be2b1d7be
tcostello74 <tcostello74@yahoo.com>
2023-05-29 09:24:16 -0400

    part 2

--- models.py
+++ models.py
@@ -1,13 +1,9 @@
 """Models for Blogly."""
-
+import datetime
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-def connect_db(app):
-    """Connect to database"""
-    db.app = app
-    db.init_app(app)
 
 class User(db.Model):
     """USER"""
@@ -22,4 +18,31 @@ class User(db.Model):
     last_name = db.Column(db.String,
                           nullable=False)
     image_url = db.Column(db.String,
-                          nullable=True)    
+                          nullable=True)
+
+    posts = db.relationship("Post", backref="user", cascade="all, delete-orphan")    
+
+class Post(db.Model):
+    """Blog post"""
+
+    __tablename__ = "posts"
+
+    id = db.Column(db.Integer,
+                   primary_key=True)
+    title = db.Column(db.Text,
+                      nullable=False)
+    content = db.Column(db.Text,
+                        nullable=False)
+    created_at =db.Column(db.Datetime,
+                          nullable=False,
+                          default=datetime.datetime.now)
+    user_id = db.Column(db.Integer,
+                        db.Foreign_key('users.id'),
+                        nullable=False)
+
+
+def connect_db(app):
+    """Connect to database"""
+    
+    db.app = app
+    db.init_app(app)
